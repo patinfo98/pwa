8d9c2b142cd2e8797f0d4e366205aa50c0e90767
Dana Vlas <dana-monica.vlas@edu.fh-joanneum.at>
2024-12-03 16:27:55 +0100

    Add mock tests and verify Jest configuration

---
+++ pollyPWA-master/tests/mock/mock.test.js
@@ -0,0 +1,9 @@
+describe("Mock Tests", () => {
+  test("should pass a basic math test", () => {
+    expect(2 + 2).toBe(4);
+  });
+
+  test("should pass a string manipulation test", () => {
+    expect("Hello World".toLowerCase()).toBe("hello world");
+  });
+});
